c1ac429f4dce5572a5794ad19aa43a8b8d87ed68
HassanNaeem2001 <hassannaeem4590@gmail.com>
2025-06-02 17:50:48 +0500

    02nd Junw 2025 - Gsap added

--- gsap.js
+++ gsap.js
@@ -10,3 +10,9 @@ gsap.from('.linkedin-icon',{
     opacity: 0,
     ease: 'power2.out',
 })
+gsap.from('.navbardivpositionwala',{
+    duration: 1.5,
+    y: -100,
+    opacity: 0,
+    ease: 'power2.out',
+})
