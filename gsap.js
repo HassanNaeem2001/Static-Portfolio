1b8e544cf857f1cb60dc95c02198141b81ad59f2
HassanNaeem2001 <hassannaeem4590@gmail.com>
2025-06-02 15:02:38 +0500

    GSAP Added

---
+++ gsap.js
@@ -0,0 +1,12 @@
+gsap.from('.whatsapp-icon',{
+    duration: 1.5,
+    x: 100,
+    opacity: 0,
+    ease: 'power2.out',
+})
+gsap.from('.linkedin-icon',{
+    duration: 1.5,
+    x: -100,
+    opacity: 0,
+    ease: 'power2.out',
+})
